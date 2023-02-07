b12b7bb2d959d9fbd3077fc8c4ba452ff13efccd
Carlos Villarreal <Carlos.Villarreal@solera.com>
2023-02-07 15:46:35 +0100

    adding public and private routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,30 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
   return (
     <div>
         <Routes>
-          <Route path='login' element={<LoginPage />} />
-          <Route path='/*' element={<HeroesRoutes />} />
+          <Route path='login/*' element={
+            <PublicRoute>
+              {/*<LoginPage />*/}
+              <Routes>
+                <Route path="/*" element={<LoginPage />} />
+              </Routes>
+            </PublicRoute>
+            } 
+
+            />
+
+          <Route path="/*" element={
+            <PrivateRoute>
+              <HeroesRoutes />
+            </PrivateRoute>
+          }
+          />
         </Routes>
     </div>
   );
--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,14 +1,18 @@
+import { useContext } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/context/AuthContext";
 
 export const Navbar = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const { user, logout } = useContext(AuthContext);
 
-    const onLogout = () => {
-        navigate('/login', {
-            replace: true
-        });
-    }
+  const onLogout = () => {
+    navigate("/login", {
+      replace: true,
+    });
+    logout();
+  };
 
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
@@ -48,11 +52,10 @@ export const Navbar = () => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-primary">Carlos</span>
-          <button 
-          className="nav-item nav-link btn"
-          onClick={ onLogout }
-          >Logout</button>
+          <span className="nav-item nav-link text-primary">{user?.name}</span>
+          <button className="nav-item nav-link btn" onClick={onLogout}>
+            Logout
+          </button>
         </ul>
       </div>
     </nav>
